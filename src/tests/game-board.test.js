5e64adebf8805c1d4fe7e9e69e27c2cba3af7b84
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-13 13:14:59 -0300

    test: add default board creation unit

--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -1,2 +1,8 @@
 import { describe, expect, test } from "vitest";
-import { GameBoard } from "../game-board";
+import { createBoard, GameBoard } from "../game-board";
+
+describe("Board creator", () => {
+  test("creates a 1 by 1 board by default", () => {
+    expect(createBoard()).toStrictEqual([[0]]);
+  });
+});
