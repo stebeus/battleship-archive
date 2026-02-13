601cf8a37c73625283ad11eed484ceb1c4c58a19
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-13 14:53:11 -0300

    test: move board creator units to a separate file

--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -1,26 +1,6 @@
 import { beforeEach, describe, expect, test } from "vitest";
 import { createBoard, GameBoard } from "../game-board";
 
-describe("Board creator", () => {
-  const board = createBoard(10, 10);
-
-  test("creates a 1 by 1 board by default", () => {
-    expect(createBoard()).toStrictEqual([[0]]);
-  });
-
-  test("board has 10 rows", () => {
-    expect(board).toHaveLength(10);
-  });
-
-  test("board has 10 columns", () => {
-    expect(board[0]).toHaveLength(10);
-  });
-
-  test("all empty cell values are 0", () => {
-    expect(board.every((row) => row.every((cell) => cell === 0)));
-  });
-});
-
 const gameBoard = new GameBoard();
 const destroyer = gameBoard.ships[4];
 
