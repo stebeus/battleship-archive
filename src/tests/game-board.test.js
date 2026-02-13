69cbaab03fa0f76cc1e1f7c0b0af7759f62b4a92
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-13 13:17:21 -0300

    test: add board row quantity unit

--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -2,7 +2,13 @@ import { describe, expect, test } from "vitest";
 import { createBoard, GameBoard } from "../game-board";
 
 describe("Board creator", () => {
+  const board = createBoard(10, 10);
+
   test("creates a 1 by 1 board by default", () => {
     expect(createBoard()).toStrictEqual([[0]]);
+  });
+
+  test("board has 10 rows", () => {
+    expect(board).toHaveLength(10);
   });
 });
