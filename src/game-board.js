5b6541a44eea3824d5a1ba2bcba2b6f4e245dfba
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-13 13:24:45 -0300

    feat: create board creator

---
+++ src/game-board.js
@@ -0,0 +1,15 @@
+function createBoard(rows = 1, columns = 1) {
+  const board = [];
+  const cell = 0;
+
+  for (let row = 0; row < rows; row++) {
+    board[row] = [];
+    for (let column = 0; column < columns; column++) {
+      board[row][column] = cell;
+    }
+  }
+
+  return board;
+}
+
+export { createBoard };
