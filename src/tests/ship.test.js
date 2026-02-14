513e3170dc5edc5ca4f065c5aff2c9d033c4700b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-14 15:10:13 -0300

    test: group hit units

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -11,16 +11,18 @@ test("creates ship of length 1 by default", () => {
   expect(ship).toHaveLength(1);
 });
 
-test("reduces ship health by 1 hit", () => {
-  ship.health = 2;
-  ship.hit();
-  expect(ship.health).toBe(1);
-});
+describe("Ship.hit", () => {
+  test("reduces ship health by 1 hit", () => {
+    ship.health = 2;
+    ship.hit();
+    expect(ship.health).toBe(1);
+  });
 
-test("stops reducing ship health when it is 0", () => {
-  ship.health = 0;
-  ship.hit();
-  expect(ship.health).toBe(0);
+  test("stops reducing ship health when it is 0", () => {
+    ship.health = 0;
+    ship.hit();
+    expect(ship.health).toBe(0);
+  });
 });
 
 describe("Ship.isWrecked", () => {
