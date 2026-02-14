18bc604969d5309171dec4fda5d2f6a5d5e0835a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-14 13:41:52 -0300

    style: represent numbers as digits

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -7,11 +7,11 @@ beforeEach(() => {
   ship = new Ship();
 });
 
-test("creates a ship of length one by default", () => {
+test("creates ship of length 1 by default", () => {
   expect(ship).toHaveLength(1);
 });
 
-test("reduces ship health by one hit", () => {
+test("reduces ship health by 1 hit", () => {
   ship.hit();
   expect(ship.health).toBe(0);
 });
