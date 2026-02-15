383f26a136fd24ff295006ff106faba6e1518e00
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-15 13:10:23 -0300

    test: add default flotilla length unit

--- src/tests/fleet.test.js
+++ src/tests/fleet.test.js
@@ -1,1 +1,8 @@
 import { expect, test } from "vitest";
+import { createFlotilla } from "../fleet";
+
+const flotilla = createFlotilla();
+
+test("creates a flotilla of 1 ship by default", () => {
+  expect(flotilla).toHaveLength(1);
+});
