6b8d25c2e8dbe63316c864b0e7e7ce28ae8cba88
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-15 13:38:48 -0300

    test: group flotilla units

--- src/tests/fleet.test.js
+++ src/tests/fleet.test.js
@@ -1,32 +1,34 @@
 import { beforeEach, describe, expect, test } from "vitest";
 import { createFlotilla } from "../fleet";
 
-let flotilla;
+describe("Flotilla", () => {
+  let flotilla;
 
-describe("Default behavior", () => {
+  describe("Default behavior", () => {
+    beforeEach(() => {
+      flotilla = createFlotilla();
+    });
+
+    test("creates a flotilla of 1 ship by default", () => {
+      expect(flotilla).toHaveLength(1);
+    });
+
+    test("ship length is 1", () => {
+      const isLengthOne = (ship) => ship.length === 1;
+      expect(flotilla.every(isLengthOne)).toBeTruthy();
+    });
+  });
+
   beforeEach(() => {
-    flotilla = createFlotilla();
+    flotilla = createFlotilla(5, 5);
   });
 
-  test("creates a flotilla of 1 ship by default", () => {
-    expect(flotilla).toHaveLength(1);
+  test("creates a flotilla of 5 ships", () => {
+    expect(flotilla).toHaveLength(5);
   });
 
-  test("ship length is 1", () => {
-    const isLengthOne = (ship) => ship.length === 1;
-    expect(flotilla.every(isLengthOne)).toBeTruthy();
+  test("all ship lengths are 5", () => {
+    const isLengthFive = (ship) => ship.length === 5;
+    expect(flotilla.every(isLengthFive)).toBeTruthy();
   });
-});
-
-beforeEach(() => {
-  flotilla = createFlotilla(5, 5);
-});
-
-test("creates a flotilla of 5 ships", () => {
-  expect(flotilla).toHaveLength(5);
-});
-
-test("all ship lengths are 5", () => {
-  const isLengthFive = (ship) => ship.length === 5;
-  expect(flotilla.every(isLengthFive)).toBeTruthy();
 });
