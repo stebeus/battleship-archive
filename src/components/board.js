d31ccae91b9c11d10278caefb7dc152219928348
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-18 07:54:38 -0300

    feat: create grid factory

--- src/components/board.js
+++ src/components/board.js
@@ -1,1 +1,20 @@
 const template = document.getElementById("board-template");
+
+function createGrid(element, grid) {
+  const rows = grid.length;
+  const columns = grid[0].length;
+
+  for (let row = 0; row < rows; row++) {
+    for (let column = 0; column < columns; column++) {
+      const cell = element.querySelector(".grid__cell").cloneNode(true);
+
+      const position = `${row},${column}`;
+      cell.dataset.coordinates = position;
+
+      element.append(cell);
+    }
+  }
+
+  element.firstElementChild.remove();
+  return element;
+}
