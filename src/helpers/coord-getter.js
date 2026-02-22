d746dd8674884444d9c4d22bdb59d27dec7cbb0b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 10:53:11 -0300

    refactor: use element as the input instead of the event target

--- src/helpers/coord-getter.js
+++ src/helpers/coord-getter.js
@@ -1,8 +1,5 @@
-function getCoordinates(event) {
-  const cell = event.target.closest(".grid__cell");
-  if (!cell) return;
-
-  const dataCoords = cell.dataset.coords;
+function getCoordinates(element) {
+  const dataCoords = element.dataset.coords;
   const coordinates = dataCoords.split(",").map(Number);
 
   return coordinates;
